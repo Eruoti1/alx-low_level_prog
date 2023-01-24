c5c20145b48a2b8cdbd9997a7ee9942ac7625753
Eruoti1 <kevinombui49@gmail.com>
2023-01-24 00:14:15 -0800

    hello

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -32,8 +32,8 @@ typedef struct printer
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
+ll(const char * const format, ...);
 void print_all(const char * const format, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
-#endif
-
+#endif /* VARIADIC_FUNCTIONS_H */
