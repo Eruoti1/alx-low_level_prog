62e091b235d0ee54893dbbcf4038a07eb36eab6d
Eruoti1 <kevinombui49@gmail.com>
2023-01-30 21:41:12 -0800

    function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,40 +1,27 @@
-#ifndef LISTS_H
+#include "lists.h"
 
-#define LISTS_H
-/**
- * File: lists.h
- *
- * Auth: Brennan D Baraban
- *
- * Desc: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x11-singly_linked_lists directory.
- */
 
-#include <stdlib.h>
 
 /**
- * struct list_s - singly linked list
+ * print_list - prints the elements of a list
  *
- * @str: string - (malloc'ed string)
+ * @h: pointer to the first element in the list
  *
- * @len: length of the string
- *
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- *            for Holberton project
+ * Return: the number of nodes
  */
-typedef struct list_s
+size_t print_list(const list_t *h)
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	int i = 0;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
-#endif /* LISTS_H */
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
